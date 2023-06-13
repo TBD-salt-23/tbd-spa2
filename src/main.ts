f2fcbd49052b019d4622278c5dbfb6176e13ea6c
Mihaela Antoci <mihaela.antoci@appliedtechnology.se>
2023-06-13 14:17:29 +0200

    Feat: functional search bar

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import './style.css';
-const template = document.getElementById('template');
+const template = document.querySelector('template') as Node;
 const source = 'https://picsum.photos/200';
 const listOfImages = [
   source,
@@ -16,13 +16,40 @@ const listOfImages = [
 
 const showImg = () => {
   listOfImages.forEach(() => {
-    const imgClone = document.importNode(template, true);
+    const imgClone = document.importNode(template, true) as any; //CHANGE THIS AT SOME POINT
     imgClone.content.querySelector('.image').setAttribute('src', source);
     const clone = imgClone.content.cloneNode(true);
-    document.querySelector('.imageCards').append(clone);
+    document.querySelector('.imageCards')?.append(clone);
   });
 };
 
 showImg();
+const searchHistory: string[] = [];
+const storageObject = {
+  history: [],
+};
+
+type Storage = {
+  history: string[];
+};
+const button = document.querySelector('.searchBtn') as HTMLButtonElement;
+button.addEventListener('click', () => {
+  const searchParam = document.querySelector(
+    '.searchBar__field'
+  ) as HTMLInputElement;
+  if (!searchParam.value) return;
+  let storedItems = localStorage.getItem('searchParam') as any;
+  if (!storedItems) {
+    localStorage.setItem('searchParam', JSON.stringify({ history: [] }));
+    storedItems = localStorage.getItem('searchParam') as any;
+  }
+
+  const parsedStorage = JSON.parse(storedItems) as Storage;
+  parsedStorage.history.push(searchParam.value);
+  const storageToSave = JSON.stringify(parsedStorage);
+  searchHistory.push(searchParam.value);
+
+  localStorage.setItem('searchParam', storageToSave);
+});
 
 /* eslint-disable */
